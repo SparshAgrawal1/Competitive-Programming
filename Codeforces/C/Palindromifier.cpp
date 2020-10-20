c32650d20e91475843482a36dc995cb5caa19778
SparshAgrawal1 <56772551+SparshAgrawal1@users.noreply.github.com>
2020-10-20 17:47:13 +0530

    Initial commit

---
+++ Codeforces/C/Palindromifier.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <string>
+#include <cmath>
+#include <map>
+#include <bitset>
+#define MAX 1000
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+using namespace std;
+typedef long long int ll;
+ 
+ 
+int main()
+{
+    fast;
+    ll t=1;
+ 
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        ll n=s.length();
+        cout<<3<<endl;
+        cout<<"R "<<n-1<<endl;
+        cout<<"L "<<n<<endl;
+        cout<<"L "<<2<<endl;
+ 
+    }
+    return 0;
+}
